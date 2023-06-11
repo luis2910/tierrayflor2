1e1b8e5af2112295c79fa7b937c4ccef19b30ea7
luis2910 <lui.perezc@duocuc.cl>
2023-06-10 22:20:21 -0400

    base

--- tierrayflor/settings.py
+++ tierrayflor/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'tierrayflor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'mydatabase',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '1521',
+        'SID': 'your_sid',
     }
 }
 
